9baccfcaa46af11f099f1b4dd7140e0ddf6bfcd4
Victor Avila <avilavictor1570@gmail.com>
2020-08-06 04:07:38 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,9 +4,9 @@ class Appointment
   @@all = []
   
   def initialize(date, patient, doctor)
+    @doctor = doctor
     @date = date
     @patient = patient
-    @doctor = doctor
     @@all << self
   end
   
